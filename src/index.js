dbbdd20f2f16f72f0540e0c6a3e2968ab9c8b508
Edson Michaque <edson@michaque.com>
2020-07-02 22:04:02 +0200

    Handle access token generation from client configuration

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import { Environment } from './environment.js'
-import { Operation } from './operation.js'
-import { Configuration } from './configuration.js'
-import { Client } from './client.js'
-
-export { Environment, Operation, Configuration, Client }
+import { Environment } from './environment.js';
+import { Operation } from './operation.js';
+import { Configuration } from './configuration.js';
+import { Client } from './client.js';
+import { AccessToken } from './security.js';
+export { 
+	Environment,
+	Operation,
+	Configuration,
+	Client,
+	AccessToken
+};
