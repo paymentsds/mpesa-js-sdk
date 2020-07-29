a9b1e4b873f16fa8acb7723760f9b236c5c74872
Elton Laice <me@eltonlaice.com>
2020-07-30 01:30:38 +0200

    Send Money to a Business Account Test

--- test/client.js
+++ test/client.js
@@ -10,7 +10,7 @@ const client = new Client({
     serviceProviderCode: '<REPLACE>' // input_ServiceProviderCode
  });
 
- const client = new Client({
+ const clientRevert = new Client({
     apiKey: '<REPLACE>',              // API Key
     publicKey: '<REPLACE>',           // Public Key
     serviceProviderCode: '<REPLACE>', // input_ServiceProviderCode,
@@ -33,13 +33,20 @@ const paymentDataSend = {
 };
 
 const reversionData = {
-    reference: '11114',           // input_ThirdPartyReference
-    transation: 'T12344CC',       // input_TransactionID
-    amount: '10'                  // input_ReversalAmount
+    reference: '11114',               // input_ThirdPartyReference
+    transation: 'T12344CC',           // input_TransactionID
+    amount: '10'                      // input_ReversalAmount
+ };
+
+ const paymentDataBusiness = {
+    to: '979797',                    // input_ReceiverPartyCode
+    reference: '11114',              // input_ThirdPartyReference
+    transation: 'T12344CC',          // input_TransactionReference
+    amount: '10'                     // input_Amount
  };
 
 describe("Receive Money from a Mobile Account", function(){
-    it("Receive Money successful", function(){
+    it("Receive Money successful", function(done){
         client.receive(paymentDataReceive).then(r => {
             expect(r.response.status).to.be.within(200, 201);
             done();
@@ -50,7 +57,7 @@ describe("Receive Money from a Mobile Account", function(){
 });
 
 describe("Send Money to a Mobile Account", function(){
-    it("Send Money successful", function(){
+    it("Send Money successful", function(done){
         client.send(paymentDataSend).then(r => {
             expect(r.response.status).to.be.within(200, 201);
             done();
@@ -60,9 +67,31 @@ describe("Send Money to a Mobile Account", function(){
     });
 });
 
+describe("Send Money to a Mobile Account", function(){
+    it("Send Money successful", function(done){
+        client.send(paymentDataSend).then(r => {
+            expect(r.response.status).to.be.within(200, 201);
+            done();
+         }).catch(e =>{
+            done(new Error("test case failed: " + e));
+         });
+    });
+});
+
+describe("Send Money to a Business Account ", function(done){
+    it("Send Money successful", function(done){
+        client.send(paymentDataBusiness).then(r => {
+            expect(r.response.status).to.be.within(200, 201);
+            done();
+        }).catch(e =>{
+            done(new Error("test case failed: " + e));
+        });
+    });
+});
+
 describe("Revert a Transaction ", function(){
     it("Revert a Transaction successful", function(){
-        client.revert(reversionData).then(function(r) {
+        clientRevert.revert(reversionData).then(function(r) {
             expect(r.response.status).to.be.within(200, 201);
          }).catch(function(e) {
             done(new Error("test case failed: " + e));
